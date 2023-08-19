398d8baf9d1f211e3eaab9ce80e9447d2fe05fa7
jdramirezl <juliandavid.ramirez@mercadolibre.com.co>
2023-08-19 18:43:37 -0500

    Initial commit for talleres folder and first numeral of taller 1

---
+++ talleres/taller1/resources/views/home/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page - Online Store')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Clase: Base Drone </div>
+
+                <div class="card-body text-center">
+                    <h1>Welcome to Our Homepage</h1>
+                    <p>Elija a que punto quiere ir</p>
+
+                    <div class="mt-3">
+                        <a href="#" class="btn btn-primary">Button 1</a>
+                        <a href="#" class="btn btn-secondary">Button 2</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ talleres/taller1/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Your Laravel App</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+
+</html>
