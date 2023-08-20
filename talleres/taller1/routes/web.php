1bdf166896c91cd9b5a41aed8ec24c33a6a89a2b
jdramirezl <juliandavid.ramirez@mercadolibre.com.co>
2023-08-19 22:34:10 -0500

    fixed message error

--- talleres/taller1/routes/web.php
+++ talleres/taller1/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('/drones', 'App\Http\Controllers\DroneController@index')->name('drones.index');
-Route::get('/drones/save', 'App\Http\Controllers\DroneController@store')->name('drones.store');
+Route::post('/drones/save', 'App\Http\Controllers\DroneController@store')->name('drones.store');
 Route::get('/drones/create', 'App\Http\Controllers\DroneController@create')->name('drones.create');
 Route::get('/drones/{id}', 'App\Http\Controllers\DroneController@show')->name('drones.show');
 Route::delete('/drones/{id}', 'App\Http\Controllers\DroneController@destroy')->name('drones.destroy');
